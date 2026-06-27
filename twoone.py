58a20dd7d350d41e95d1ecea34eee4b9e2652aed
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-27 21:05:08 +0530

    Label encoder used

--- twoone.py
+++ twoone.py
@@ -2,3 +2,12 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 df = pd.read_csv("encoding_database.csv") #change file later
+
+df.head()
+
+y=df["Purchaswd"]
+label_enc= LabelEncoder()
+y_enc= label_enc.fit_transform(y)
+
+y_enc
+label_enc.inverse_transform(y_enc)
