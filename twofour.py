646b5d7428a18a1117b77b01d12c7cf4ad62a1ea
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-13 17:18:59 +0530

    mistake corrected and code completed

--- twofour.py
+++ twofour.py
@@ -1,7 +1,8 @@
-from sklearn import make_regression
+from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
 
-x,y=make_regression(n_feaures=1,noise=5,n_samples=5000,random_state=0)
+x,y=make_regression(n_features=1,noise=5,n_samples=5000,random_state=0)
 print(x)
 print(y)
-plt.scatter(x,y)
+plt.scatter(x,y,s=5)
+plt.show()
