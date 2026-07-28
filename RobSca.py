3fb821a14222a043bbd18e28e0fe43098addb151
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-28 14:20:48 +0530

    base element required

--- RobSca.py
+++ RobSca.py
@@ -2,3 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import RobustScaler
 
+data = np.array([10,11,12,13,14,15,100]).reshape(-1,1)
+
+rob_scaled= RobustScaler().fit_transform(data)
+
