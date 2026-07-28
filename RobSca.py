ee1e8d6ae62608063dd0f19d8d3bcdf7e8d57e9b
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-28 14:24:03 +0530

    original data

--- RobSca.py
+++ RobSca.py
@@ -6,3 +6,9 @@ data = np.array([10,11,12,13,14,15,100]).reshape(-1,1)
 
 rob_scaled= RobustScaler().fit_transform(data)
 
+plt.figure(figsize=(12,5))
+plt.subplot(1,3,1)
+plt.scatter(range(len(data)),data,color="blue")
+plt.title("Original Data")
+plt.ylabel("Value")
+
