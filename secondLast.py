d77c3a5eb71c4ea1f44ba8dbf4d2a1080c26cb1f
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-30 14:22:40 +0530

    accuracy score completed

--- secondLast.py
+++ secondLast.py
@@ -2,3 +2,13 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+iris=load_iris()
+X_train,X_test,y_train,y_test=train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)
+
+clf=LogisticRegression(max_iter=200)
+clf.fit(X_train,y_train)
+print(clf.score(X_test,y_test))
+
+y_pred=clf.predict(X_test)
+print(accuracy_score(y_pred, y_test))
